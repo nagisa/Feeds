28adc53d506c090bf508c5ca50bb46f5c89ae5d9
Simonas Kazlauskas <simonas@kazlauskas.me>
2012-09-22 23:57:54 +0300

    Include a file, I forgot to add long time ago

---
+++ lightread/data/db_init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE items (id INTEGER PRIMARY KEY,
+                    title TEXT,
+                    author VARCHAR(1024),
+                    summary VARCHAR(141),
+                    href VARCHAR(1024),
+                    time FLOAT,
+                    subscription VARCHAR(1024),
+                    unread BOOLEAN,
+                    starred BOOLEAN);
+
+CREATE TABLE subscriptions (id VARCHAR(1024),
+                            url VARCHAR(1024),
+                            title VARCHAR(1024));
+
+CREATE TABLE labels (id INTEGER,
+                     name VARCHAR(1024),
+                     subscriptions VARCHAR(1024));
